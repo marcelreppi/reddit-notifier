215d29356dfd9a8daf3074ea747a7512d08d94c7
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-03-19 20:46:03 +0100

    added telegram bot

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bot = require('./bot.js')
+
 const { fetchRSSFeed, sendNotification } = require('./lib.js')
 
 const subreddits = require('./subreddits.json').subreddits
@@ -17,8 +19,19 @@ async function checkReddit() {
 }
 
 exports.handler = async (event) => {
-  await checkReddit()
-  // await sendNotification('Test', ['Serverless reddit-notifier is working']) // For testing
+  try {
+    await checkReddit()
+    // await sendNotification('Test', ['Serverless reddit-notifier is working']) // For testing
+    await bot.telegram.sendMessage(process.env.MY_CHAT_ID, 'Serverless reddit-notifier: Successfully executed')
+  } catch (error) {
+    await bot.telegram.sendMessage(process.env.MY_CHAT_ID, 'Serverless reddit-notifier: There was an error!\n\n' + String(error))
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+    return response;
+  }
+  
   const response = {
     statusCode: 200,
     body: JSON.stringify('Reddalert executed'),
